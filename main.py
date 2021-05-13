560d6b22a69055108a19860ccbc3dbf9afc1354b
Alex Tawney <>
2021-05-13 16:58:24 +0000

    some questions and madlibs elements added

---
+++ main.py
@@ -0,0 +1,75 @@
+import time
+import turtle
+
+t = turtle
+
+firstquestion = input(str('What is your name? '))
+secondquestion = input(str('Hi ' + firstquestion + ', How is your day going? '))
+if(secondquestion == 'Good'):
+    thirdquestion = input(str('Why was your day Good? '))
+elif(secondquestion == 'good'):
+  thirdquestion = input(str('Why was your day good? '))
+elif(secondquestion == 'bad'):
+    thirdquestion = input(str('Why was your day bad? '))
+elif(secondquestion == 'Bad'):
+    thirdquestion = input(str('Why was your day Bad? '))
+else:
+  thirdquestion = input(str('Why was your day ' + secondquestion + '? ' ))
+fourthquestion = input(str('Is there anything that I can do to make it better for you? '))
+print('Oh well I cant exactly help with that because im a robot')
+
+time.sleep(2.5)
+
+color = input(str('Anyways ' + firstquestion + ' whats your favorite color? '))
+
+askdraw = input(str('Would you like me to draw something with the color you said? '))
+if(askdraw == 'Yes'):
+  t.color(color)
+  t.circle(30)
+  t.forward(40)
+  t.backward(80)
+  t.left(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+elif(askdraw == 'yes'):
+  t.color(color)
+  t.circle(30)
+  t.forward(40)
+  t.backward(80)
+  t.left(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+elif(askdraw == 'y'):
+  t.color(color)
+  t.circle(30)
+  t.forward(40)
+  t.backward(80)
+  t.left(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+elif(askdraw == 'Y'):
+  t.color(color)
+  t.circle(30)
+  t.forward(40)
+  t.backward(80)
+  t.left(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+  t.right(90)
+  t.forward(80)
+else:
+  print(' ')
+
+  
+
+
