3789dc1bf8bdc55e4ce83d56cc3972fe6f0cee6d
Alex Tawney <>
2021-05-16 23:04:07 +0000

    added more questions and did some over the weekend work

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ elif(secondquestion == 'Bad'):
     thirdquestion = input(str('Why was your day Bad? '))
 else:
   thirdquestion = input(str('Why was your day ' + secondquestion + '? ' ))
-fourthquestion = input(str('Is there anything that I can do to make it better for you? '))
-print('Oh well I cant exactly help with that because im a robot')
+fourthquestion = input(str('(send a few word response) Is there anything that I can do to make it better for you? '))
+print('Sorry I cant help with ' + fourthquestion + ' I am just a robot')
 
 time.sleep(2.5)
 
@@ -48,7 +48,7 @@ elif(askdraw == 'yes'):
   t.right(90)
   t.forward(80)
   time.sleep(2)
-  askfriends = input(str('Wow! I love the color ' + color + '! so anyways do you have any friends?'))
+  askfriends = input(str('Wow! I love the color ' + color + '! so anyways do you have friends? '))
 elif(askdraw == 'y'):
   t.color(color)
   t.circle(30)
@@ -76,15 +76,31 @@ elif(askdraw == 'Y'):
   time.sleep(2)
   askfriends = input(str('Wow! I love the color ' + color + '! so anyways do you have any friends?'))
 else:
-  askaboutfriends = input(str('The color ' + color + '! is very nice, anyways do you have any friends yes or no? '))
+  askfriends = input(str('The color ' + color + '! is very nice, anyways do you have any friends yes or no? '))
 
-if(askaboutfriends == 'Yes'):
+if(askfriends == 'Yes'):
   askfriendsnames = input(str('Oh thats good what are your friends names?'))
-elif(askaboutfriends == 'yes'):
+elif(askfriends == 'yes'):
   askfriendsnames = input(str('Oh thats good what are your friends names?'))
-elif(askaboutfriends == 'Y'):
+elif(askfriends == 'Y'):
   askfriendsnames = input(str('Oh thats good what are your friends names?'))
-elif(askaboutfriends == 'y'):
+elif(askfriends == 'y'):
   askfriendsnames = input(str('Oh thats good what are your friends names?'))
 else:
-  askfriendsnames = input(str('Oh well if you dont have any friends I would be your friend if i wasnt a robot'))
+  askfriendsnames = input(str('Oh well what is a few of your family members names? '))
+listfriendsnamesaskcrime = input(str('The people you listed names are ' + askfriendsnames + ' and they are very nice names! Anyways just curious have you ever commited a crime? '))
+
+if(listfriendsnamesaskcrime == 'Yes'):
+  askwhatcrime = input(str(firstquestion + ' WOAH YOUR GOING TO JAIL, wait, wait wait, what crime? '))
+elif(listfriendsnamesaskcrime == 'yes'):
+  askwhatcrime = input(str(firstquestion + ' WOAH YOUR GOING TO JAIL, wait, wait wait, what crime? '))
+elif(listfriendsnamesaskcrime == 'Y'):
+  askwhatcrime = input(str(firstquestion + ' WOAH YOUR GOING TO JAIL, wait, wait wait, what crime? '))
+elif(listfriendsnamesaskcrime == 'y'):
+  askwhatcrime = input(str(firstquestion + ' WOAH YOUR GOING TO JAIL, wait, wait wait, what crime? '))
+else:
+  askwhatcrime = input(str('Im a lie detector bot aswell as a question bot I can tell you are lying tell me what crime!?'))
+
+thatsaseriouscrime = input(str('WOAH!! ' + askwhatcrime + ' IS A VERY SERIOUS CRIME... would you do it again? '))
+if(thatsaseriouscrime == 'Yes'):
+  wouldyoudoitagain = input(str('WHY? ' + askwhatcrime + ' IS A VERY SERIOUS FEDERAL CRIME!!!, but I wont judge '))
